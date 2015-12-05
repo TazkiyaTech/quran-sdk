2059d1c0ce5d4c71a9e87566314cdfe6c564a382
Adil Hussain <adil@thinkincode.co.uk>
2015-12-05 19:24:29 +0000

    Re-added *QuranDatabaseHelperTest.testGetAyahsInSurah()*.

--- QuranUtils/QuranUtilsLibrary/src/androidTest/java/com/thinkincode/quranutils/database/QuranDatabaseHelperTest.java
+++ QuranUtils/QuranUtilsLibrary/src/androidTest/java/com/thinkincode/quranutils/database/QuranDatabaseHelperTest.java
@@ -1,6 +1,7 @@
 package com.thinkincode.quranutils.database;
 
 import com.thinkincode.quranutils.BaseTestCase;
+import com.thinkincode.quranutils.database.model.SurahEnum;
 
 import java.util.List;
 
@@ -17,12 +18,14 @@ public class QuranDatabaseHelperTest extends BaseTestCase {
 	public void testGetAyahsInSurah() {
 		// go through and test method call for each surah of the Qur'an
 
-//		for (int i = 0; i < ChapterEnum.values().length; i++) {
-//			// When.
-//			List<String> ayahsInSurah = QuranTesterApplication.getInstance().getQuranDatabaseHelper().getAyahsInSurah(i + 1);
-//
-//			// Then.
-//			assertEquals(ChapterEnum.values()[i].getNumVerses(), ayahsInSurah.size());
-//		}
+		SurahEnum[] surahs = SurahEnum.values();
+
+		for (int i = 1; i <= surahs.length; i++) {
+			// When.
+			List<String> ayahsInSurah = getQuranDatabaseHelper().getAyahsInSurah(getContext(), i);
+
+			// Then.
+			assertEquals(surahs[i - 1].getNumVerses(), ayahsInSurah.size());
+		}
 	}
 }
