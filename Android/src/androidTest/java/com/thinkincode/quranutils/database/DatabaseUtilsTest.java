2c5c35d8d826a46ccb971e8f4219f5783a29882e
Adil Hussain <adil@thinkincode.co.uk>
2016-03-11 19:51:24 +0000

    Added tests to the *DatabaseUtilsTest* class.

--- Android/src/androidTest/java/com/thinkincode/quranutils/database/DatabaseUtilsTest.java
+++ Android/src/androidTest/java/com/thinkincode/quranutils/database/DatabaseUtilsTest.java
@@ -30,14 +30,91 @@ public class DatabaseUtilsTest extends BaseTestCase {
     }
 
     @Test
-    public void test_explainQueryPlanForSelectStatement() {
-        // Given.
-        String tableName = QuranDatabaseHelper.TABLE_NAME_SURA_NAMES;
-
+    public void test_explainQueryPlanForSelectStatement_when_tableNameIsSuraNames_and_noWhereClauseProvided() {
         // When.
-        QueryPlan result = DatabaseUtils.explainQueryPlanForSelectStatement(sqLiteDatabase, tableName, null, null, null, null, null, null, null);
+        QueryPlan result = DatabaseUtils.explainQueryPlanForSelectStatement(
+                sqLiteDatabase,
+                QuranDatabaseHelper.TABLE_NAME_SURA_NAMES,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null);
 
         // Then.
-        assertThat(result.getDetail(), is(equalTo("SCAN TABLE " + tableName)));
+        assertThat(result.getDetail(), is(equalTo("SCAN TABLE sura_names")));
+    }
+
+    @Test
+    public void test_explainQueryPlanForSelectStatement_when_tableNameIsSuraNames_and_whereClauseProvided() {
+        // When.
+        QueryPlan result = DatabaseUtils.explainQueryPlanForSelectStatement(sqLiteDatabase,
+                QuranDatabaseHelper.TABLE_NAME_SURA_NAMES,
+                null,
+                QuranDatabaseHelper.COLUMN_NAME_SURA + " = ? ",
+                new String[] { "1" },
+                null,
+                null,
+                null,
+                null);
+
+        // Then.
+        assertThat(result.getDetail(), is(equalTo("SEARCH TABLE sura_names USING INDEX index_sura_number_on_table_sura_names (sura=?)")));
+    }
+
+    @Test
+    public void test_explainQueryPlanForSelectStatement_when_tableNameIsQuranText_and_noWhereClauseProvided() {
+        // When.
+        QueryPlan result = DatabaseUtils.explainQueryPlanForSelectStatement(
+                sqLiteDatabase,
+                QuranDatabaseHelper.TABLE_NAME_QURAN_TEXT,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        // Then.
+        assertThat(result.getDetail(), is(equalTo("SCAN TABLE quran_text")));
+    }
+
+    @Test
+    public void test_explainQueryPlanForSelectStatement_when_tableNameIsQuranText_and_whereClauseProvided_1() {
+        // When.
+        QueryPlan result = DatabaseUtils.explainQueryPlanForSelectStatement(sqLiteDatabase,
+                QuranDatabaseHelper.TABLE_NAME_QURAN_TEXT,
+                null,
+                QuranDatabaseHelper.COLUMN_NAME_SURA + " = ? ",
+                new String[] { "1" },
+                null,
+                null,
+                null,
+                null);
+
+        // Then.
+        assertThat(result.getDetail(), is(equalTo("SEARCH TABLE quran_text USING INDEX index_sura_number_on_table_quran_text (sura=?)")));
+    }
+
+    @Test
+    public void test_explainQueryPlanForSelectStatement_when_tableNameIsQuranText_and_whereClauseProvided_2() {
+        // When.
+        QueryPlan result = DatabaseUtils.explainQueryPlanForSelectStatement(sqLiteDatabase,
+                QuranDatabaseHelper.TABLE_NAME_QURAN_TEXT,
+                null,
+                QuranDatabaseHelper.COLUMN_NAME_SURA + " = ? " +
+                        " AND " +
+                        QuranDatabaseHelper.COLUMN_NAME_AYA + " = ? ",
+                new String[] { "1", "1" },
+                null,
+                null,
+                null,
+                null);
+
+        // Then.
+        assertThat(result.getDetail(), is(equalTo("SEARCH TABLE quran_text USING INDEX index_sura_number_aya_number_on_table_quran_text (sura=? AND aya=?)")));
     }
 }
