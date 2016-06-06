8eded35c5acd164fa083b244b924981b7a3044fe
Adil Hussain <adil@thinkincode.co.uk>
2016-06-06 01:50:49 +0100

    Removed Javadoc comment from *ExampleUnitTest* file.

--- Android/src/test/java/com/thinkincode/quran/sdk/ExampleUnitTest.java
+++ Android/src/test/java/com/thinkincode/quran/sdk/ExampleUnitTest.java
@@ -4,9 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
 public class ExampleUnitTest {
 
     @Test
