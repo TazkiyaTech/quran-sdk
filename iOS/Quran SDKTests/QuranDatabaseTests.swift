36d32365541ca580864304ea537198b98491cc3d
Adil Hussain <adil.hussain@masabi.com>
2018-11-26 12:27:16 +0000

    First working version of the iOS SDK.

---
+++ iOS/Quran SDKTests/QuranDatabaseTests.swift
@@ -0,0 +1,57 @@
+//
+//  Quran_SDKTests.swift
+//  Quran SDKTests
+//
+//  Created by Adil Hussain on 23/11/2018.
+//  Copyright © 2018 Tazkiya Tech. All rights reserved.
+//
+
+import XCTest
+@testable import Quran_SDK
+
+class QuranDatabaseTests: XCTestCase {
+
+    private var quranDatabase: QuranDatabase?
+    
+    override func setUp() {
+        quranDatabase = QuranDatabase()
+    }
+
+    override func tearDown() {
+        try! quranDatabase!.closeDatabase()
+    }
+    
+    func test_getSurahNames() throws {
+        // Given.
+        let expected = ["الفاتحة", "البقرة", "آل عمران", "النساء", "المائدة", "الأنعام", "الأعراف", "الأنفال", "التوبة", "يونس", "هود", "يوسف", "الرعد", "ابراهيم", "الحجر", "النحل", "الإسراء", "الكهف", "مريم", "طه", "الأنبياء", "الحج", "المؤمنون", "النور", "الفرقان", "الشعراء", "النمل", "القصص", "العنكبوت", "الروم", "لقمان", "السجدة", "الأحزاب", "سبإ", "فاطر", "يس", "الصافات", "ص", "الزمر", "غافر", "فصلت", "الشورى", "الزخرف", "الدخان", "الجاثية", "الأحقاف", "محمد", "الفتح", "الحجرات", "ق", "الذاريات", "الطور", "النجم", "القمر", "الرحمن", "الواقعة", "الحديد", "المجادلة", "الحشر", "الممتحنة", "الصف", "الجمعة", "المنافقون", "التغابن", "الطلاق", "التحريم", "الملك", "القلم", "الحاقة", "المعارج", "نوح", "الجن", "المزمل", "المدثر", "القيامة", "الانسان", "المرسلات", "النبإ", "النازعات", "عبس", "التكوير", "الإنفطار", "المطففين", "الإنشقاق", "البروج", "الطارق", "الأعلى", "الغاشية", "الفجر", "البلد", "الشمس", "الليل", "الضحى", "الشرح", "التين", "العلق", "القدر", "البينة", "الزلزلة", "العاديات", "القارعة", "التكاثر", "العصر", "الهمزة", "الفيل", "قريش", "الماعون", "الكوثر", "الكافرون", "النصر", "المسد", "الإخلاص", "الفلق", "الناس"]
+        
+        // When.
+        let actual = try quranDatabase!.getSurahNames()
+        
+        // Then.
+        XCTAssertEqual(expected, actual);
+    }
+    
+    func test_getSurahName() throws {
+        XCTAssertEqual("الفاتحة", try quranDatabase!.getSurahName(1));
+    }
+    
+    func test_getAyahsInSurah() throws {
+        // Given.
+        let expected = ["بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ", "الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ", "الرَّحْمَٰنِ الرَّحِيمِ", "مَالِكِ يَوْمِ الدِّينِ", "إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ", "اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ", "صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّالِّينَ"]
+        
+        // When.
+        let actual = try quranDatabase!.getAyahsInSurah(1)
+        
+        // Then.
+        XCTAssertEqual(expected, actual);
+    }
+    
+    func test_getAyah() throws {
+        // When.
+        let actual = try quranDatabase!.getAyah(surahNumber: 1, ayahNumber: 1)
+        
+        // Then.
+        XCTAssertEqual("بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ", actual);
+    }
+}
