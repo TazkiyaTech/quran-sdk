55ef4b98a1b4c4da02555367ff9e917cd42c0bbc
Adil Hussain <adil.hussain@masabi.com>
2019-05-11 00:27:16 +0100

    Converted `HifdhTips` from Java to Kotlin.

---
+++ Android/src/main/java/com/tazkiyatech/quran/sdk/database/HifdhTips.kt
@@ -0,0 +1,43 @@
+package com.tazkiyatech.quran.sdk.database
+
+import android.content.res.Resources
+
+import com.tazkiyatech.quran.sdk.R
+
+import java.util.Random
+
+/**
+ * Helper class which provides Hifdh tips.
+ *
+ * @property resources The application's [Resources] instance.
+ */
+class HifdhTips(private val resources: Resources) {
+
+    private val random: Random = Random()
+
+    /**
+     * @return a randomly selected Hifdh tip.
+     */
+    val nextRandom: String
+        get() = getTip(random.nextInt(size))
+
+    /**
+     * The number of Hifdh tips available.
+     */
+    internal val size: Int
+        get() = tipsArray.size
+
+    /**
+     * The [Array] of Hifdh tips.
+     */
+    private val tipsArray: Array<String>
+        get() = resources.getStringArray(R.array.hifdh_tips)
+
+    /**
+     * @param index the index of the Hifdh tip to get.
+     * @return the Hifdh tip at the specified index.
+     */
+    internal fun getTip(index: Int): String {
+        return tipsArray[index]
+    }
+}
