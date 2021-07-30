cc63a9375dbcdf88dcf65297b3510b615a644ada
Adil Hussain <adilson05uk@gmail.com>
2021-07-30 16:14:25 +0100

    (iOS) Raised the minimum deployment target version for the iOS platform from iOS 8.0 to iOS 9.0.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "QuranSDK",
     platforms: [
-        .iOS(.v8), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)
+        .iOS(.v9), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
